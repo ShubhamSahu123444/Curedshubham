efc02204d70a4b199582d344505cb730b24ed91e
aakash kumar sahu <aakashcol93@hotmail.com>
2022-02-24 17:22:20 +0530

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Curedshubham.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Contact { get; set; }
+        public Location Location { get; set; }
+    }
+}
